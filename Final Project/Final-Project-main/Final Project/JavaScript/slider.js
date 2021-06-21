bdd888417c780c345ffb3ce9110a828cd24140cf
aayushh-patell <76638947+aayushh-patell@users.noreply.github.com>
2021-06-20 23:59:04 -0500

    Add files via upload

---
+++ Final Project/Final-Project-main/Final Project/JavaScript/slider.js
@@ -0,0 +1,29 @@
+// Essentially this JavaScript code receives an input from the next or previous buttons on the slideshow that signal
+// for the designated functions plusSlides and showSlides to display the next image if the next button is clicked
+// or to display the previous image if the previous button is pressed.
+// https://www.w3schools.com/howto/howto_js_slideshow.asp
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("slides");
+
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+
+    slides[slideIndex-1].style.display = "block";  
+}
